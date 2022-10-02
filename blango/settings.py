a68338355485065871a462351253ecabd24e85d4
Jivan Guragain <g.jivanphysics@gmail.com>
2022-10-02 03:17:50 +0000

    Added pagination and django filter

--- blango/settings.py
+++ blango/settings.py
@@ -115,6 +115,7 @@ class Dev(Configuration):
       'rest_framework',
       'rest_framework.authtoken',
       'drf_yasg',
+      'django_filters',
   ]
   # REST_FRAMEWORK = {
   #   'DEFAULT_RENDERER_CLASSES': [
@@ -140,6 +141,12 @@ class Dev(Configuration):
         "user_sustained": "5000/day",
         "user_burst": "100/minute",
     },
+    "DEFAULT_PAGINATION_CLASS":"rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE":100,
+    "DEFAULT_FILTER_BACKENDS":[
+      "django_filters.rest_framework.DjangoFilterBackend",
+      "rest_framework.filters.OrderingFilter",
+    ],
   }
 
   SWAGGER_SETTINGS={
